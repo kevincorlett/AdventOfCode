a1d1a6b404ed2bbd4c36e838a55d57c23e55184e
Kevin Corlett <kevin@debian.home>
2020-12-09 18:52:37 +0000

    2020 8.2 done
    Committer: Kevin Corlett

--- 2020/8.2/index.js
+++ 2020/8.2/index.js
@@ -1,15 +1,20 @@
 const fs = require('fs');
 
 const hrstart = process.hrtime.bigint();
-const data = fs.readFileSync("./test.txt").toString();
+const data = fs.readFileSync("./input.txt").toString();
 const hrload = process.hrtime.bigint();
 
-const items = data.split('\n').map(x => ({ op: x.substring(0, 3), value: parseInt(x.substring(4)), done: 0, flipped: false }));
+const items = data.split('\n').map(x => ({
+    op: x.substring(0, 3),
+    value: parseInt(x.substring(4)),
+    done: 0,
+    flipped: false
+}));
 const hrParse = process.hrtime.bigint();
 
-const flippage = { jmp: 'nop', nop: 'jmp', acc: 'acc' };
+const flippers = { jmp: 'nop', nop: 'jmp', acc: 'acc' };
 const flip = (item) => {
-    item.op = flippage[item.op];
+    item.op = flippers[item.op];
     item.flipped = true;
 }
 
@@ -18,26 +23,23 @@ let total = 0;
 let flipped = null;
 let attempt = 1;
 while (i < items.length) {
-    process.stdout.write(`\n${i} `);
     if (items[i].done == attempt) {
-        process.stdout.write(`been here before `);
         attempt++;
         i = 0;
         total = 0;
         flip(flipped);
+        flipped = null;
+        continue;
     }
 
     items[i].done = attempt;
     switch (items[i].op) {
         case 'acc':
-            process.stdout.write(`${total} `);
             total += items[i].value;
-            process.stdout.write(`${total} acc ${items[i].value}`);
             i++;
             break;
         case 'jmp':
-            if (!items[i].flipped){
-                process.stdout.write(`flipping jpm to nop `);
+            if (!flipped && !items[i].flipped) {
                 flip(items[i]);
                 flipped = items[i];
                 items[i].done = 0;
@@ -46,8 +48,7 @@ while (i < items.length) {
             }
             break;
         case 'nop':
-            if (!items[i].flipped){
-                process.stdout.write(`flipping nop to jmp ${items[i].value}`);
+            if (!flipped && !items[i].flipped) {
                 flip(items[i]);
                 flipped = items[i];
                 items[i].done = 0;
@@ -61,7 +62,7 @@ while (i < items.length) {
 }
 const hrProcess = process.hrtime.bigint();
 
-console.log("acc =", total);
+console.log("\nacc =", total);
 
 console.log("Load\t\t", (hrload - hrstart) / 1000n, "μs")
 console.log("Parse\t\t", (hrParse - hrload) / 1000n, "μs");
