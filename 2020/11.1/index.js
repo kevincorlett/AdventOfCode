0a7e642444e32360007b27ba209120f7c746e51f
Kevin Corlett <kevin.corlett@kinetic-solutions.co.uk>
2020-12-12 20:07:37 +0000

    2020 11.1

--- 2020/11.1/index.js
+++ 2020/11.1/index.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 
 const hrstart = process.hrtime.bigint();
-const data = fs.readFileSync("./test.txt");
+const data = fs.readFileSync("./input.txt");
 const hrload = process.hrtime.bigint();
 
-const EMPTY = 'L', OCCUPIED = '#', FLOOR = '.';
+const NULL = 0, EMPTY = 1, OCCUPIED = 2, FLOOR = 3 ;
+const rowSize = data.indexOf('\n');
+let seats = new Array(rowSize + 3).fill(NULL);
 
-let seats = [];
 for (let i = 0; i < data.length; i++) {
     const x = data.readUInt8(i);
     switch (x) {
@@ -20,24 +21,41 @@ for (let i = 0; i < data.length; i++) {
             seats.push(OCCUPIED);
             break;
         case 0x0A: // \n
+            seats.push(NULL, NULL);
         default:
         //noop
     }
 }
+seats.push(...new Array(rowSize + 3).fill(NULL));
 const hrParse = process.hrtime.bigint();
 
 //relative indexes of the surrounding locations
-const surroundings = [-11, -10, -9, -1, 1, 9, 10, 11];
+const surroundings = [-rowSize-3, -rowSize - 2, -rowSize - 1, -1, 1, rowSize + 1, rowSize + 2, rowSize + 3];
 
-let next = new Array(seats.length);
-let currentStr = '', nextStr = '';
-let result = 0;
-do{
-    currentStr = seats.join('');
-console.log(currentStr);
+function countSurroundingOccupants (i){
+    let occupiedCount = 0;
+    for (let j = 0; j < surroundings.length; j++) {
+        const check = i + surroundings[j];
+        if (check < 0)
+            continue;
+        if (check >= seats.length)
+            break;
+        if (seats[check] === OCCUPIED) {
+            occupiedCount++;
+        }
+        if (occupiedCount > 3)
+            break;
+    }
+    return occupiedCount;
+}
 
-    result++;
-    // process.stdout.write(`\r${result}`);
+
+let next = seats;
+
+do {
+    seats = next;
+    next = new Array(seats.length);
+
     for (let i = 0; i < seats.length; i++) {
 
         // floor is floor
@@ -47,19 +65,7 @@ console.log(currentStr);
         }
 
         //look for occupied surrounding
-        let occupiedCount = 0;
-        for (let j = 0; j < surroundings.length; j++) {
-            const check = i + surroundings[j];
-            if (check < 0)
-                continue;
-            if (check >= seats.length)
-                break;
-            if (seats[check] === OCCUPIED) {
-                occupiedCount++;
-            }
-            if (occupiedCount > 3)
-                break;
-        }
+        let occupiedCount = countSurroundingOccupants(i);
 
         // if a seat is empty and there are no occupied seats adjacent it becomes occupied
         if (seats[i] === EMPTY && occupiedCount === 0) {
@@ -72,16 +78,22 @@ console.log(currentStr);
         }
 
         // otherwise, the seat's state doesn't change
-        else{
+        else {
             next[i] = seats[i];
         }
     }
-    nextStr = next.join('');
-    seats = next;
-    next = new Array(next.length);
-    console.log(nextStr);
-    if (result > 5) break;
-} while (currentStr !== nextStr);
+
+} while (!arrComp(seats,next));
+
+function arrComp(arr1,arr2){
+    for (let i=0;i<arr1.length && i<arr2.length;i++){
+        if (arr1[i] !== arr2[i])
+            return false;
+    }
+    return true;
+}
+
+const result = seats.filter(x => x === OCCUPIED).length;
 
 const hrProcess = process.hrtime.bigint();
 
