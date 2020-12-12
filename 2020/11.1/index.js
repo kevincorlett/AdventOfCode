00866297d914be2f3c5c65058e358215d90453fc
Kevin Corlett <kevinscorlett@hotmail.com>
2020-12-12 16:31:12 +0000

    2020 11.1 wip

---
+++ 2020/11.1/index.js
@@ -0,0 +1,93 @@
+const fs = require('fs');
+
+const hrstart = process.hrtime.bigint();
+const data = fs.readFileSync("./test.txt");
+const hrload = process.hrtime.bigint();
+
+const EMPTY = 'L', OCCUPIED = '#', FLOOR = '.';
+
+let seats = [];
+for (let i = 0; i < data.length; i++) {
+    const x = data.readUInt8(i);
+    switch (x) {
+        case 0x4C: // L
+            seats.push(EMPTY);
+            break;
+        case 0x2E: // .
+            seats.push(FLOOR);
+            break;
+        case 0x23: // #
+            seats.push(OCCUPIED);
+            break;
+        case 0x0A: // \n
+        default:
+        //noop
+    }
+}
+const hrParse = process.hrtime.bigint();
+
+//relative indexes of the surrounding locations
+const surroundings = [-11, -10, -9, -1, 1, 9, 10, 11];
+
+let next = new Array(seats.length);
+let currentStr = '', nextStr = '';
+let result = 0;
+do{
+    currentStr = seats.join('');
+console.log(currentStr);
+
+    result++;
+    // process.stdout.write(`\r${result}`);
+    for (let i = 0; i < seats.length; i++) {
+
+        // floor is floor
+        if (seats[i] === FLOOR) {
+            next[i] = FLOOR;
+            continue;
+        }
+
+        //look for occupied surrounding
+        let occupiedCount = 0;
+        for (let j = 0; j < surroundings.length; j++) {
+            const check = i + surroundings[j];
+            if (check < 0)
+                continue;
+            if (check >= seats.length)
+                break;
+            if (seats[check] === OCCUPIED) {
+                occupiedCount++;
+            }
+            if (occupiedCount > 3)
+                break;
+        }
+
+        // if a seat is empty and there are no occupied seats adjacent it becomes occupied
+        if (seats[i] === EMPTY && occupiedCount === 0) {
+            next[i] = OCCUPIED;
+        }
+
+        // if a seat is occupied and four or more adjacent seats are also occupied, it becomes empty
+        else if (seats[i] === OCCUPIED && occupiedCount > 3) {
+            next[i] = EMPTY;
+        }
+
+        // otherwise, the seat's state doesn't change
+        else{
+            next[i] = seats[i];
+        }
+    }
+    nextStr = next.join('');
+    seats = next;
+    next = new Array(next.length);
+    console.log(nextStr);
+    if (result > 5) break;
+} while (currentStr !== nextStr);
+
+const hrProcess = process.hrtime.bigint();
+
+console.log("result =", result);
+
+console.log("Load\t\t", (hrload - hrstart) / 1000n, "μs")
+console.log("Parse\t\t", (hrParse - hrload) / 1000n, "μs");
+console.log("Process\t\t", (hrProcess - hrParse) / 1000n, "μs");
+console.log("Total\t\t", (hrProcess - hrstart) / 1000n, "μs");
