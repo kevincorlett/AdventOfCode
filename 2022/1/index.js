8ac0dbe4db0b9f26fbb0af53172917e470878885
Kevin Corlett <kevin.corlett@goji.investments>
2022-12-01 14:07:29 +0000

    Removed unused vars

--- 2022/1/index.js
+++ 2022/1/index.js
@@ -3,7 +3,7 @@ const input = require('fs').readFileSync('input.txt').toString();
 const inputLines = input.split('\n');
 
 //part 1
-let max = 0, current = 0, index = 0, result = 0;
+let max = 0, current = 0;
 for (let i = 0; i <= inputLines.length; i++) {
     if (inputLines[i] === '' || inputLines[i] === undefined) {
         if (current > max) {
